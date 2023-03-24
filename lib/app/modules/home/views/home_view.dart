047f9c7f7712dae0de8b7cfbc159d48b5238fb52
AyikwatiMuhfiyati <80374601+AyikwatiMuhfiyati@users.noreply.github.com>
2023-03-25 00:10:55 +0700

    Add files via upload

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,66 @@
+import 'dart:js_util';
+
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  double heightDevice = Get.height;
+  double widthDevice = Get.width;
+
+  @override
+  Widget build(BuildContext context) {
+    //GETX Fitur
+    double paddingTop = context.mediaQueryPadding.top;
+    double paddingBottom = context.mediaQueryPadding.bottom;
+
+    AppBar myAppBar() {
+      return AppBar(
+        title: Text("HOME"),
+      );
+    }
+
+    double heightBody =
+        context.height - myAppBar().preferredSize.height - paddingTop;
+
+    return Scaffold(
+      appBar: myAppBar(),
+      body: Container(
+        width: Get.width,
+        height: Get.height,
+        color: Colors.amber,
+        child: LayoutBuilder(builder: (context, constraints) {
+          double lebarKuning = constraints.maxWidth;
+          double tinggiKuning = constraints.maxHeight;
+          return Center(
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(70),
+                  width: lebarKuning * 0.6,
+                  height: tinggiKuning * 0.5,
+                  color: Colors.red,
+                )
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'HomeView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
